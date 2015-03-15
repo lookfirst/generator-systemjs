ffc01d0a4242e5ffd2506c76b5856b5f133ececb
Jon Stevens <latchkey@gmail.com>
2015-03-14 19:17:31 -0700

    back to using es5 version of ocll-router

--- templates/app/src/system.config.js
+++ templates/app/src/system.config.js
@@ -20,7 +20,7 @@ System.config({
     "json": "github:systemjs/plugin-json@0.1.0",
     "ngreact": "github:davidchang/ngReact@0.1.4",
     "oclazyload": "github:ocombe/ocLazyLoad@0.6.3",
-    "oclazyload-systemjs-router": "github:lookfirst/oclazyload-systemjs-router@1.1.4",
+    "oclazyload-systemjs-router": "github:lookfirst/oclazyload-systemjs-router@1.1.5",
     "react": "npm:react@0.13.0",
     "todomvc-app-css": "github:tastejs/todomvc-app-css@1.0.3",
     "todomvc-common": "github:tastejs/todomvc-common@1.0.1",
@@ -93,7 +93,7 @@ System.config({
     "github:jspm/nodelibs-vm@0.1.0": {
       "vm-browserify": "npm:vm-browserify@0.0.4"
     },
-    "github:lookfirst/oclazyload-systemjs-router@1.1.4": {
+    "github:lookfirst/oclazyload-systemjs-router@1.1.5": {
       "angular": "github:angular/bower-angular@1.3.14",
       "angular-ui-router": "github:angular-ui/ui-router@0.2.13",
       "oclazyload": "github:ocombe/ocLazyLoad@0.6.3",
