49fc9d25621d6ea9f9ae764e8c0e1801f6c55ac5
Jon Stevens <latchkey@gmail.com>
2015-04-03 16:47:38 -0700

    latest systemjs/jspm

--- templates/app/karma.conf.js
+++ templates/app/karma.conf.js
@@ -1,72 +1,76 @@
 // Karma configuration
 
 module.exports = function(config) {
-  config.set({
+	config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+		// base path that will be used to resolve all patterns (eg. files, exclude)
+		basePath: '',
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jspm', 'jasmine'],
+		// frameworks to use
+		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+		frameworks: ['jspm', 'jasmine'],
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+		// start these browsers
+		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+		browsers: ['Chrome'],
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'verbose', 'coverage'],
+		// test results reporter to use
+		// possible values: 'dots', 'progress'
+		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
+		reporters: ['progress', 'verbose', 'coverage'],
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
+		// Continuous Integration mode
+		// if true, Karma captures browsers, runs the tests and exits
+		singleRun: true,
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+		// enable / disable colors in the output (reporters and logs)
+		colors: true,
 
-    // list of files / patterns to load in the browser
-    files: [],
+		// list of files / patterns to load in the browser
+		files: [],
 
-    jspm: {
-      // Edit this to your needs
-      config: 'src/system.config.js',
-      loadFiles: [
-        'jspm_packages/npm/babel@*/external-helpers.js',
-        'test-unit/**/*.js'
-      ],
-      serveFiles: [
-        'dist/**/**',
-        'jspm_packages/**/*.js',
-        'jspm_packages/**/*.css'
-      ]
-    },
+		jspm: {
+			// Edit this to your needs
+			config: 'system.config.js',
+			loadFiles: [
+				'test-unit/**/*.js'
+			],
+			serveFiles: [
+				'dist/**/**'
+			]
+		},
 
-    proxies: {
-      '/base/app': '/base/dist/app',
-      '/base/common': '/base/dist/common',
-      '/jspm_packages': '/base/jspm_packages'
-    },
+		proxies: {
+			'/base/app': '/base/dist/app',
+			'/base/common': '/base/dist/common',
+			'/jspm_packages': '/base/jspm_packages'
+		},
 
-    // list of files to exclude
-    exclude: [],
+		// list of files to exclude
+		exclude: [],
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+		// level of logging
+		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+		logLevel: config.LOG_INFO,
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      'test-unit/**/*.js': ['babel', 'coverage']
-    },
+		// preprocess matching files before serving them to the browser
+		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+		preprocessors: {
+			'test-unit/**/*.js': ['babel', 'coverage']
+		},
 
-    // optionally, configure the reporter
-    coverageReporter: {
-      type : 'html',
-      dir : 'coverage/'
-    }
+		'babelPreprocessor': {
+			options: {
+				sourceMap: 'inline',
+				modules: 'system'
+			}
+		},
 
-  });
+		// optionally, configure the reporter
+		coverageReporter: {
+			type : 'html',
+			dir : 'coverage/'
+		}
+
+	});
 };
